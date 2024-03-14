0afdb31f1d379f289b1d35ef9b7d68a429248064
Karlon Auger <karlonauger@gmail.com>
2024-03-14 16:39:43 -0400

    Test

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -10,6 +10,9 @@ export default function useFetch(url) {
       setLoading(true);
 
       try {
+        console.log(process.env);
+        console.log(process.env.REACT_APP_API_URL);
+        console.log(process.env.REACT_APP_API_TOKEN);
         const res = await fetch(process.env.REACT_APP_API_URL + "/api/" + url, {
           headers: {
             Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
