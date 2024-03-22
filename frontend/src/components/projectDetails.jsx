20ff61ba6068af43c9f986c8fe7ed83b97b60b9c
Karlon Auger <karlonauger@gmail.com>
2024-03-22 15:07:31 -0400

    Remove extra Button dependency

--- frontend/src/components/projectDetails.jsx
+++ frontend/src/components/projectDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Modal, Carousel, Button } from 'react-bootstrap';
+import { Modal, Carousel } from 'react-bootstrap';
 
 import Navbar from './navbar';
 
