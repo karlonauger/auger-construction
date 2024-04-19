7a00d1a98eb5d4056cd3978b96384dccc9d329ae
Karlon Auger <karlonauger@gmail.com>
2024-04-19 15:20:20 -0400

    Add google Anylitics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/home';
+import GoogleAnalytics from './components/googleAnalytics';
 import ProjectDetails from './components/projectDetails';
 import Footer from './components/footer';
 
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <div className="App">
       <Router>
+        <GoogleAnalytics />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects/:projectId" element={<ProjectDetails />} />
