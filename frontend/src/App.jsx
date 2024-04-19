8e6d46847195a313271bf597b3c3b3b40aabe0b8
Karlon Auger <karlonauger@gmail.com>
2024-04-19 15:50:54 -0400

    Fix Google Analytics cookie security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 import Home from './components/home';
-import GoogleAnalytics from './components/googleAnalytics';
 import ProjectDetails from './components/projectDetails';
 import Footer from './components/footer';
+
+ReactGA.initialize(process.env.REACT_APP_GA_ID, {
+  send_page_view: true,
+  gtagOptions: { cookie_flags: 'max-age=7200;secure;samesite=none' }
+});
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <GoogleAnalytics />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects/:projectId" element={<ProjectDetails />} />
